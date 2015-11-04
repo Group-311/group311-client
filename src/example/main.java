778597c5b41d6070cfd9d46ea79ee5e705ea62ff
DonCarle <ccarls14@student.aau.dk>
2015-11-04 09:37:31 +0100

    This is the client.

---
+++ src/example/main.java
@@ -0,0 +1,51 @@
+package example;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		DatagramSocket socket;
+		
+		
+		try
+		{	
+			socket = new DatagramSocket(); 												//Make a socket
+			
+			String message = "BigAssCunt"; 												//Creates the message
+			
+			byte[] b = message.getBytes(); 												//Send the message to be made of bytes
+			
+			InetAddress host = InetAddress.getByName("172.20.10.3");  					//Put name of the server here
+	
+			int serverSocket = 2000; 													//name of the port
+			
+			DatagramPacket request = new DatagramPacket(b,b.length,host,serverSocket); 	//Creating packet
+			
+			socket.send(request); 														//sending the packet
+			
+			
+			//******************************************************************
+			byte [] buffer = new byte[1000]; 
+			
+			DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+			
+			socket.receive(reply);
+			
+			System.out.println(new String(reply.getData()));
+			
+			
+			socket.close();
+		
+		}
+		catch (Exception ex)
+		{
+			
+		}
+	}
+
+}
+
