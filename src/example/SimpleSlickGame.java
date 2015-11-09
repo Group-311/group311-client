11940d0dfd4f098f442148b2dd30a8683a967bfe
DonCarle <ccarls14@student.aau.dk>
2015-11-09 13:34:25 +0100

    Added map reference.

--- src/example/SimpleSlickGame.java
+++ src/example/SimpleSlickGame.java
@@ -1,33 +1,53 @@
 package example;
 import java.util.logging.Level;
+import org.lwjgl.input.Mouse;
 import java.util.logging.Logger;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.ImageBuffer;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.opengl.LoadableImageData;
+
+import com.sun.javafx.collections.MappingChange.Map;
+
+import sun.net.www.content.image.gif;
 
 public class SimpleSlickGame extends BasicGame
 {
 	public SimpleSlickGame(String gamename)
 	{
-		super(gamename);
+		super("TicketToRide");
 	}
 
 	@Override
 	public void init(GameContainer gc) throws SlickException {}
 
 	@Override
-	public void update(GameContainer gc, int i) throws SlickException {}
+	public void update(GameContainer gc, int i) throws SlickException 
+	{
+		Input input = gc.getInput();
+		int xpos = Mouse.getX();
+		int ypos = Mouse.getY();
+		
+		if(input.isMousePressed(0))
+		{
+			System.out.println("x: " + xpos + "        y: " +ypos);
+		
+		}
+		
+	}
 
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
-		for(int i = 0; i < 10; i++){
-		g.drawString("Stop, siger Kirsten!", 2.5f*i*10, 2*i*10);
-		}
-	
+	Image map = new Image("/Map.jpg");		//Loads the placement Map image used to detect cities
+	g.drawImage(map, 0, 0);					//Place it in (0,0)
 	}
+	
 
 	public static void main(String[] args)
 	{
@@ -35,7 +55,7 @@ public class SimpleSlickGame extends BasicGame
 		{
 			AppGameContainer appgc;
 			appgc = new AppGameContainer(new SimpleSlickGame("Simple Slick Game"));
-			appgc.setDisplayMode(640, 480, false);
+			appgc.setDisplayMode(1024, 768, false);
 			appgc.start();
 		}
 		catch (SlickException ex)
