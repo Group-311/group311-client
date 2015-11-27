6d106bcfb797c11f10f917b40e443b33586a9c0f
DonCarle <ccarls14@student.aau.dk>
2015-11-27 12:32:53 +0100

    Pushing cards

--- src/example/Card.java
+++ src/example/Card.java
@@ -59,12 +59,12 @@ protected Image displayedImage;
 		else
 			frontImage.draw(xPos,yPos,width,height);
 	}
-	public Color getColor() {
-		return color;
-	}
-
+	
 	public void setColor(Color color) {
 		this.color = color;
+	}
+	public Color getColor() {
+		return color;
 	}
 
 }
--- src/example/TrainCard.java
+++ src/example/TrainCard.java
@@ -2,36 +2,39 @@ package example;
 
 public class TrainCard extends Card {
 
-	protected Color color;
 
-	
-	public TrainCard(){
-		
+
+	public TrainCard() {
+
 	}
-	
-	public TrainCard(Color _color){
-		
-		this.color = _color;
 
-		
-		//this.frontImage = trainImage;
+	public TrainCard(Color color) {
+
+		this.color =color;
+
+		// this.frontImage = trainImage;
 	}
-	
 
 
 
-	public void playCard(){
-		
-		
-		
+	public void playCard() {
+
 		/**
-		 * if selected connection color == this.color or this.color == joker color or
-		 * selected color == grey
-		 * then decrease these cards from the TrainCardHandStack
-		 * Add these cards to the TrainTrashStack
-		 * Make the connection isTaken true by calling makeTaken();
-		 * Give points to the player by accessing playerPiece
-		 * Move playerPiece
+		 * if selected connection color == this.color or this.color == joker
+		 * color or selected color == grey then decrease these cards from the
+		 * TrainCardHandStack Add these cards to the TrainTrashStack Make the
+		 * connection isTaken true by calling makeTaken(); Give points to the
+		 * player by accessing playerPiece Move playerPiece
 		 */
+	}
+	@Override
+	public void setColor(Color color) {
+		// TODO Auto-generated method stub
+		super.setColor(color);
+	}
+	@Override
+	public Color getColor() {
+		// TODO Auto-generated method stub
+		return super.getColor();
 	}
 }
