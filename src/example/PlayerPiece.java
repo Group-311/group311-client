6b62b662942288767fe746a0c1851e3efabf35d2
DonCarle <ccarls14@student.aau.dk>
2015-11-30 14:51:27 +0100

    Added the playerPiece class that creates a circle that moves aroudn the poitnsystem.

--- src/example/PlayerPiece.java
+++ src/example/PlayerPiece.java
@@ -1,21 +1,60 @@
 package example;
 
+import org.newdawn.slick.Color;
+import org.newdawn.slick.geom.Circle;
+
 public class PlayerPiece {
-	
+
 	private int totalPoints, xPos, yPos;
 	private Color color;
-	private Train [] trains;
-	private Connection [] completedRoutes;
-	
-	PlayerPiece(){
-		
-		completedRoutes = new Connection [36];
-		
-	}
-	
-	public void move(int points){
-		this.xPos += points;
-		this.yPos += points;
+	private Circle playerPiece;
+
+	PlayerPiece(Color color) {
+		this.color = color;
+		xPos = 18;
+		yPos = 632;
+		totalPoints = 0;
+
 	}
 
+	public void move(int points) {
+		if (xPos == 18 && yPos <= 632 && yPos >= 104) {
+			this.yPos -= points * 33;
+		}
+		if (yPos == 104 && xPos <= 843 && xPos >= 18) {
+			this.xPos += points * 33;
+		}
+		if (xPos == 876 && yPos >= 104 && yPos <= 632) {
+			this.yPos += points * 33;
+		}
+		if (yPos == 665 && xPos <= 876 && xPos > 52) {
+			this.xPos -= points * 33;
+		}
+		if (xPos < 52 && yPos == 665) {
+			xPos = 18;
+			yPos = 599;
+		}
+		totalPoints += points;
+
+	}
+
+	public void setVisible(org.newdawn.slick.Graphics g) {
+		playerPiece = new Circle(xPos, yPos, 5);
+		g.setColor(color);
+		g.fill(playerPiece);
+		g.draw(playerPiece);
+
+	}
+
+	public int getxPos() {
+		return xPos;
+	}
+
+	public int getyPos() {
+		return yPos;
+	}
+
+	public int getTotalPoints() {
+		return totalPoints;
+	}
 }
